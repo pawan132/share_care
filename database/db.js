c5c038f5e6f07134bfbdb21864942c30b5687d63
Pawan132 <pawan.2024cse1101@kiet.edu>
2024-05-17 20:26:49 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DBConnection = async () => {
+    
+
+  
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting with the database ', error.message);
+    }
+}
+
+export default DBConnection;
